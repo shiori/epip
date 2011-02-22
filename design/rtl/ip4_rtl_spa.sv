1cc8332053402be1a2c9ab34660172b283afe3eb
hustcp <none@none>
2011-02-22 23:50:51 +0800

    none

--- design/rtl/ip4_rtl_spa.sv
+++ design/rtl/ip4_rtl_spa.sv
@@ -10,18 +10,16 @@
 ///Log:
 ///Created by Andy Chen on Feb 20 2011
 
-module ip4_rtl_sp(
-  clk, op0, op1, op2, op3,
-  op, fr, nr0, nr1, lr0, lr1
+module ip4_rtl_sp import ip4_rtl_pkg::*; (
+  input logic clk,
+        wordu op0, op1, op2, op3,
+        opcode_e op,
+  output wordu fr, nr0, nr1, lr0, lr1
 );
   `include "ip4_rtl.svh"
   parameter bit has_fp_op   = 1,
                 has_lng_op  = 1,
                 stage       = 0;
-  input logic clk;
-  input wordu op0, op1, op2, op3;
-  input opcode_e op;
-  output wordu fr, nr0, nr1, lr0, lr1;
   
     
 endmodule
